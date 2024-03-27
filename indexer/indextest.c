6977f5d0fc62248bf1c2fdca5cba1a729bc8c14a
Arizbeth Rojas <98544100+Arizbethrojas@users.noreply.github.com>
2024-03-26 20:32:20 -0400

    Update indextest.c

--- indexer/indextest.c
+++ indexer/indextest.c
@@ -10,28 +10,31 @@
 #include <stdio.h>
 #include "index.h"
 
-int main(int argc, const char *argv[])
+int main(const int argc, char *argv[])
 {
     if (argc != 3)
     {
         fprintf(stderr, "Usage: indexer/indexer: oldIndexFilename newIndexFilename");
         return 1;
     }
-    char* oldIndexFilename = argv[1];
-    char* newIndexFilename = argv[2];
-    FILE* fp = fopen(newIndexFilename, "w");
-    index_t* index = indexNew(900);
-    //load oldIndexFilename into the file we will write to
-    indexLoad(oldIndexFilename, fp);
-    //write into that file
-    indexWrite(index, fp);
-    //close the file
-    fclose(fp);
-    indexDelete(index);
-
+    if (argv[1] != NULL && argv[2])
+    {
+        char *oldIndexFilename = argv[1];
+        char *newIndexFilename = argv[2];
+        FILE *old = fopen(oldIndexFilename, "r");
+        FILE *new = fopen(newIndexFilename, "w");
+        index_t *index = indexNew(900);
+        // load oldIndexFilename into the file we will write to
+        indexLoad(old);
+        // write into that file
+        indexWrite(index, new);
+        // close the file
+        fclose(old);
+        fclose(new);
+        // delete that index to avoid memory leaks
+        indexDelete(index);
+        return 0;
+    }
+    return 1; // encountered an error, return non zero
 }
 
-// Test indexer with various invalid arguments. 2. no arguments 3. one argument 4. three or more arguments 5. invalid pageDirectory (non-existent path) 5. invalid pageDirectory (not a crawler directory) 6. invalid indexFile (non-existent path) 7. invalid indexFile (read-only directory) 7. invalid indexFile (existing, read-only file)
-// Run indexer on a variety of pageDirectories and use indextest as one means of validating the resulting index.
-
-// what is the difference between index test and testing. sh?
